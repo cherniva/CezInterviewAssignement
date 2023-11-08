81c72d3c054d42729691fa9abad2ca25e8be7502
Ivan Chernikov <chernikoviivan@gmail.com>
2023-11-08 12:53:30 +0100

    Add orderbook bean and executor bean for multithreading

--- shared/src/main/java/cz/cez/trading/algo/interview/shared/OrderbookConfiguration.java
+++ shared/src/main/java/cz/cez/trading/algo/interview/shared/OrderbookConfiguration.java
@@ -1,18 +1,35 @@
 package cz.cez.trading.algo.interview.shared;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thoughtworks.qdox.model.expression.Or;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @Configuration
+@EnableAsync
 public class OrderbookConfiguration {
-
-    @Autowired
-    private ObjectMapper objectMapper;
 
     @Bean
     public JmsConsumer jmsConsumer() {
-        return new JmsConsumer(objectMapper);
+        return new JmsConsumer();
+    }
+
+    @Bean
+    public Orderbook orderbook() {
+        return new OrderbookImpl();
+    }
+
+    @Bean
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        int availableProcessors = Runtime.getRuntime().availableProcessors();
+        executor.setMaxPoolSize(availableProcessors/2 > 0 ? availableProcessors : 1); // use half of cores
+
+        return executor;
     }
 }
